e111d6290904715703dc9e1e63c6e65b570427b7
Robert McConnell <robert.mcconnell@corp.cccomm.net>
2015-08-12 14:05:15 -0700

    Finished updating the durability. Working on a better way of printing out equipment and resources

--- Game.py
+++ Game.py
@@ -11,31 +11,32 @@ from Map import mapLoad
 from Vars import gameVars
 import durability
 
-print(os.getcwd())
-print("1.To change location;\n2.Create a totally fresh map or any other key to skip it\n  ")
+#print(os.getcwd())
 
-Opt = input()
-#while(Opt != '1' or Opt != '2'):
-if Opt == '1':
-	mapResetter.mapCreator(os.getcwd())
-	mapLoad.load(os.getcwd())
-elif Opt == '2':
-	mapFresh.mapfreshCreator(os.getcwd())
-	mapLoad.load(os.getcwd())
-elif Opt == '3':
-	retVal = durability.decreaseDurability(1,0,0,0,0,0)
-	if(retVal == True) :
-		print("we have durability!")
+Opt = "-1"
+while(Opt != '1' and Opt != '2'):
+	print("1.To change location;\n2.Create a totally fresh map or any other key to skip it\n  ")
+	Opt = raw_input()
+	if Opt == '1':
+		mapResetter.mapCreator(os.getcwd())
+		mapLoad.load(os.getcwd())
+	elif Opt == '2':
+		mapFresh.mapfreshCreator(os.getcwd())
+		mapLoad.load(os.getcwd())
+	#elif Opt == '3':
+	#	retVal = durability.decreaseDurability(1,1,0,0,0,0,0)
+	#	if(retVal == True) :
+	#		print("we have durability!")
+	#	else:
+	#		print("no weapon!")
 	else:
-		print("no weapon!")
-else:
-	mapLoad.load(os.getcwd())
+		mapLoad.load(os.getcwd())
 
 #	Opt = input()
 	
 	
 toSpawn = 0
-print(Opt);
+
 while gameVars.misc_people > 0:
 	if toSpawn == 5:
 		typeSpawn = random.randint(0, 1)
@@ -47,131 +48,213 @@ while gameVars.misc_people > 0:
 			toSpawn == 0
 	
 	os.system("cls")
-	What_toDo = input ("Choose with the repective number:\n1.Chop down trees\n2.Slaughter an animal\n3.Go to a cave\n4.Check stats\n"
-					   "5.Save game\n6.Build something\n7.Craft something\n8.Smelt something\n")
-	if What_toDo == '2':
-		animalChoice = input("Do you want to kill:\n\n1.Pig\n2.Cow\n>>  ")
-		if gameVars.misc_stoneSword[0] >= 1 or gameVars.misc_ironSword[0] >= 1:
-			if gameVars.pecuary_pig >= 1 and animalChoice == '1':
-				weapon = input("What weapon would you like to use?\n\n1.Stone sword\n2.Iron sword\n>>  ")	
-				if weapon == "1" and gameVars.misc_stoneSword[0] > 0:	
-					print ("You killed a pig and managed to salvage 5kgs of meat!")
-					gameVars.consumables_food += 5
-					gameVars.pecuary_pig -= 1
-					gameVars.consumables_food -= 1
-					toSpawn += 1
-					time.sleep(3)
-					durability.decreaseDurability(1,0,0,0,0,0)
-					os.system("cls")
-				elif weapon == "2" and gameVars.misc_ironSword[0] > 0:
-					print ("You killed a pig and managed to salvage 7kgs of meat!")
-					gameVars.consumables_food += 7
-					gameVars.pecuary_pig -= 1
-					gameVars.consumables_food -= 1
-					toSpawn += 1
-					time.sleep(3)
-					durability.decreaseDurability(0,1,0,0,0,0)
-					os.system("cls")
-					
-		os.system("cls")
-		if gameVars.misc_stoneSword[0] == 0 or gameVars.misc_ironSword[0] == 0:
-			if gameVars.pecuary_pig == 0 and animalChoice == '1':
-				print ("Go back please, you do not have a sword... or pigs to kill.")
-			
-		if gameVars.misc_stoneSword[0] >= 1 or gameVars.misc_ironSword[0] >= 1:
-			if gameVars.pecuary_cow >= 1 and animalChoice == '2':
-				weapon = input("What weapon would you like to use?\n\n1.Stone sword\n2.Iron sword\n>>  ")
-				if weapon == "1" and gameVars.misc_stoneSword[0] > 0:
-					print ("You killed a cow and salvaged 7kgs of meat!")
-					gameVars.consumables_food += 7
-					gameVars.pecuary_cow -= 1
-					gameVars.consumables_food -= 1
-					toSpawn += 1
-					time.sleep(3)
-					durability.decreaseDurability(1,0,0,0,0,0)
-					os.system("cls")
-				if weapon == "2" and gameVars.misc_stoneSword[0] > 0:
-					print ("You killed a cow and salvaged 9kgs of meat!")
-					gameVars.consumables_food += 9
-					gameVars.pecuary_cow -= 1
-					gameVars.consumables_food -= 1
-					toSpawn += 1
-					time.sleep(3)
-					durability.decreaseDurability(0,1,0,0,0,0)
-					os.system("cls")
-		os.system("cls")
-		if gameVars.misc_stoneSword[0] == 0 or gameVars.misc_ironSword[0] == 0:
-			if gameVars.pecuary_cow == 0 and animalChoice == '2':
-				print("Go back please, you do not have a sword... or cows to kill.")
-		os.system("cls")
-
+	What_toDo = '-1'
+	while(What_toDo != '1' and What_toDo != '2' and What_toDo != '3' and What_toDo != '4' and What_toDo != '5' and What_toDo != '6' and What_toDo != '7' and What_toDo != '8' and What_toDo != '9'):
+		What_toDo = raw_input ("Choose with the repective number:\n1.Chop down trees\n2.Slaughter an animal\n3.Go to a cave\n4.Check stats\n5.Save game\n6.Build something\n7.Craft something\n8.Smelt something\n9.Show resources\n")
+	
+	#Refactored to use the updated durability function
+	#Updated input blocks to use while loops to prevent users from inputting bad values.
 	if What_toDo == '1':
-		if gameVars.misc_stoneAxe[0] >= 1 or gameVars.misc_ironAxe[0] >= 1:
-			which_treeChoice1 = input ("Click 'a' to chop an apple tree or 'p' to chop a pine tree>>  ")
-			if which_treeChoice1 == 'a' and gameVars.forest_appleTree >= 1:
-				tool = input("Which tool do you wish to use?\n1.Stone axe\n2.Iron axe\n>>  ")
-				if tool == "1" and gameVars.misc_stoneAxe[0] > 0:
-					print ("You chopped an apple tree, and under the leaves you found 1kg of apples and 6kgs of wood!")
-					gameVars.consumables_food += 3
-					gameVars.misc_wood += 6
-					gameVars.forest_appleTree -= 1
-					gameVars.consumables_food -= 1
-					toSpawn += 1
-					durability.decreaseDurability(0,0,1,0,0,0)
-				elif tool == "2" and gameVars.misc_stoneAxe[0] > 0:
-					print ("You chopped an apple tree, and under the leaves you found 3kgs of apples and 8kgs of wood!")
-					gameVars.consumables_food += 5
-					gameVars.misc_wood += 8
-					gameVars.forest_appleTree -= 1
-					gameVars.consumables_food -= 1
-					toSpawn += 1
-					durability.decreaseDurability(0,0,0,1,0,0)
-			elif which_treeChoice1 == 'a' and gameVars.forest_appleTree == 0:
-				print ("You do not have an apple tree to chop!")
-			if which_treeChoice1 == 'p' and gameVars.forest_pineTree >= 1:
-				print ("You have successfully chopped a pine tree! You got 7 wood points from it!")
-				gameVars.misc_wood += 7
-				gameVars.forest_pineTree -= 1
-				gameVars.consumables_food -= 1
-				toSpawn += 1
-			elif which_treeChoice1 == 'p' and gameVars.forest_pineTree == 0:
+		which_treeChoice1 = '-1'
+		while(which_treeChoice1 != '1' and which_treeChoice1 != '2'):
+			which_treeChoice1 = raw_input ("Choose with the respective number:\n1. Chop an apple tree\n2. Chop a pine tree\n")
+		if which_treeChoice1 == '1': 
+			if(gameVars.forest_appleTree > 0):
+				tool = '-1'
+				while(tool != '1' and tool != '2'):
+					tool = raw_input("Which tool do you wish to use?\n1.Stone axe\n2.Iron axe\n")
+				if tool == "1":
+					if(durability.decreaseDurability(1,0,0,1,0,0,0) == True):
+						print ("You chopped an apple tree, and under the leaves you found 1kg of apples and 6kgs of wood!")
+						gameVars.consumables_food += 3
+						gameVars.misc_wood += 6
+						gameVars.forest_appleTree -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+					else:
+						print("You don't have a Stone Axe! You need to craft another one")
+						time.sleep(3)
+						os.system("cls")
+				elif tool == "2":
+					if(durability.decreaseDurability(1,0,0,0,1,0,0) == True):
+						print ("You chopped an apple tree, and under the leaves you found 3kgs of apples and 8kgs of wood!")
+						gameVars.consumables_food += 5
+						gameVars.misc_wood += 8
+						gameVars.forest_appleTree -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+					else:
+						print("You don't have a Iron Axe! You need to craft another one")
+						time.sleep(3)
+						os.system("cls")
+			else:
+				print("There are no apple trees left!")
+				time.sleep(3)
+				os.system("cls")
+
+		elif which_treeChoice1 == '2':
+			if gameVars.forest_pineTree > 0:
+				tool = '-1'
+				while(tool != '1' and tool != '2'):
+					tool = raw_input("Which tool do you wish to use?\n1.Stone axe\n2.Iron axe\n>>  ")
+				if tool == "1":
+					if(durability.decreaseDurability(1,0,0,1,0,0,0) == True):
+						print ("You have successfully chopped a pine tree! You got 7 wood points from it!")
+						gameVars.misc_wood += 7
+						gameVars.forest_pineTree -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+					else:
+						print("You don't have a Stone Axe! You need to craft another one")
+				elif tool == "2":
+					if(durability.decreaseDurability(1,0,0,0,1,0,0) == True):
+						print ("You have successfully chopped a pine tree! You got 10 wood points from it!")
+						gameVars.misc_wood += 10
+						gameVars.forest_pineTree -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+					else:
+						print("You don't have a Iron Axe! You need to craft another one")
+			else:
 				print ("You do not have a pine tree to chop!")
-			time.sleep(3)
-			os.system("cls")
-		if gameVars.misc_stoneAxe[0] == 0 and gameVars.misc_ironAxe[0] == 0:
-			print ("You do not have an axe to chop a tree!")
-			time.sleep(3)
-			os.system("cls")
-			
+				time.sleep(3)
+				os.system("cls")
+	
+	if What_toDo == '2':
+		animalChoice = '-1'
+		while(animalChoice != '1' and animalChoice != '2'):
+			animalChoice = raw_input("Do you want to kill:\n\n1.Pig\n2.Cow\n>>  ")
+		if(animalChoice == '1'):
+			if(gameVars.pecuary_pig > 0):
+				weapon = '-1'
+				while(weapon != '1' and weapon != '2'):
+					weapon = raw_input("What weapon would you like to use?\n\n1.Stone sword\n2.Iron sword\n>>  ")	
+				if weapon == "1":
+					if durability.decreaseDurability(1,1,0,0,0,0,0) == True:
+						print ("You killed a pig and managed to salvage 5kgs of meat!")
+						gameVars.consumables_food += 5
+						gameVars.pecuary_pig -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+					else:
+						print("You don't have a stone sword! You need to craft a new one")
+						time.sleep(3)
+						os.system("cls")
+				elif weapon == "2":
+					if durability.decreaseDurability(1,0,1,0,0,0,0) == True:
+						print ("You killed a pig and managed to salvage 7kgs of meat!")
+						gameVars.consumables_food += 7
+						gameVars.pecuary_pig -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						durability.decreaseDurability(0,1,0,0,0,0)
+						os.system("cls")
+					else:
+						print("You don't have an iron sword! you need to craft a new one")
+						time.sleep(3)
+						os.system("cls")
+			else:
+				print ("Pigs are extinct!")
+	
+		#os.system("cls")
+		elif(animalChoice == '2'):				
+			if gameVars.pecuary_cow > 0:
+				weapon = '-1'
+				while(weapon != '1' and weapon != '2'):
+					weapon = raw_input("What weapon would you like to use?\n\n1.Stone sword\n2.Iron sword\n>>  ")
+				if weapon == "1":
+					if(durability.decreaseDurability(1,1,0,0,0,0,0) == True):
+						print ("You killed a cow and salvaged 7kgs of meat!")
+						gameVars.consumables_food += 7
+						gameVars.pecuary_cow -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+					else:
+						print("You don't have a stone sword! You need to craft a new one")		
+						time.sleep(3)
+						os.system("cls")			
+				if weapon == "2":
+					if(durability.decreaseDurability(1,0,1,0,0,0,0) == True):
+						print ("You killed a cow and salvaged 9kgs of meat!")
+						gameVars.consumables_food += 9
+						gameVars.pecuary_cow -= 1
+						gameVars.consumables_food -= 1
+						toSpawn += 1
+						time.sleep(3)
+						os.system("cls")
+				else:
+					print("You don't have an iron sword! you need to craft a new one")
+					time.sleep(3)
+					os.system("cls")
+			else:
+				print("Cows are extinct!")
+
 	if What_toDo == '3':
-		if gameVars.misc_stonePickaxe[0] >= 1 or gameVars.misc_ironPickaxe[0] >= 1:
-			gameVars.iron_found = random.randint(4, 9)
-			gameVars.stone_found = random.randint(2, 15)
-			print ("You have struck", gameVars.iron_found, "iron and", gameVars.stone_found, "stone!")
-			gameVars.misc_iron += gameVars.iron_found
-			gameVars.misc_stone += gameVars.stone_found
-			gameVars.consumables_food -= 3
-		elif gameVars.misc_stonePickaxe[0] == 0 or gameVars.misc_ironPickaxe[0] == 0:
-			print ("I'm sorry but you don't seem to have any pickaxes!")
-		time.sleep(3)
-		os.system("cls")
-		toSpawn += 1
-                
-	if What_toDo == '5':
-		mapSaver.saver(os.getcwd())
-		time.sleep(3)
-		os.system("cls")
+		which_pick = '-1'
+		while(which_pick != '1' and which_pick != '2'):
+			which_pick = raw_input ("Which pick would you like to use?\n1. Stone pickaxe\n2. Iron pickaxe\n")
 		
+		if(which_pick == '1'):		
+			iron_vein = random.randint(10,30)
+			stone_vein = random.randint(20,70)
+			if(durability.decreaseDurability(1,0,0,0,0,1,0) == True):
+				print ("You have struck " + str(iron_vein) + " iron and " + str(stone_vein) + " stone!")
+				
+				gameVars.misc_iron += iron_vein
+				gameVars.misc_stone += stone_vein
+				gameVars.consumables_food -= 3
+				toSpawn += 1
+				time.sleep(3)
+				os.system("cls")
+			else:
+				print("You don't have a Stone Pickaxe! You need to craft another")
+				time.sleep(3)
+				os.system("cls")
+		elif(which_pick == '2'):	
+			iron_vein = random.randint(2,6)
+			stone_vein = random.randint(4,15)
+			if(durability.decreaseDurability(1,0,0,0,0,0,1) == True):
+				print ("You have struck " + str(iron_vein) + " iron and " + str(stone_vein) + " stone!")
+				gameVars.misc_iron += iron_vein
+				gameVars.misc_stone += stone_vein
+				gameVars.consumables_food -= 3
+				toSpawn += 1
+				time.sleep(3)
+				os.system("cls")
+			else:
+				print("You don't have a Iron Pickaxe! You need to craft another")
+				time.sleep(3)
+				os.system("cls")
+
+	#Options 4, 5, 6, 7 and 8 have not been refactored.
 	if What_toDo == '4':
 		print ("You have:\nMaterials - ", gameVars.misc_stone, "stone;\n\t    ", gameVars.misc_iron, "iron;\n\t    ", gameVars.misc_ironSm, "smelted iron;\n\t    ", gameVars.misc_stoneSm, "smelted stone;\n\t    ", gameVars.misc_wood, "wood;\nHuman resources - ", gameVars.misc_people, "people;\n\t\t  ", gameVars.consumables_food, "food;\nTools - 1st index is ammount\n\t2nd index is durability\n        ", gameVars.misc_stoneAxe, "stone axe(s);\n\t",
 		gameVars.misc_ironAxe, "iron axe(s);\n\t", gameVars.misc_stoneSword, "stone sword(s);\n\t", gameVars.misc_ironSword, "iron sword(s);\n\t", gameVars.misc_stonePickaxe, "stone pickaxe(s);\n\t",
 		gameVars.misc_ironPickaxe, "iron pickaxe(s);\nMap - ", gameVars.forest_pineTree, "pine trees;\n      ", gameVars.forest_appleTree, "apple trees;\n      ", gameVars.pecuary_pig,
 		"pigs;\n      ", gameVars.pecuary_cow, "cows;\n      ", gameVars.constructions_pplHouse, "house(s);\n      ", gameVars.constructions_craftingForge, "crafting forge(s);\n      ", gameVars.constructions_prodPecuary,
 		"pecuary house(s);\n      ", gameVars.constructions_smeltingForge, "smelting forge(s).\n\n")
-		input()
+		raw_input()
+	
+	if What_toDo == '5':
+		mapSaver.saver(os.getcwd())
+		time.sleep(3)
+		os.system("cls")
 	
 	if What_toDo == '6':
-		what_toBuild = input ("What do you want to build? (choose with the respective number):\n1 A house for 29 wood;\n"
+		what_toBuild = raw_input ("What do you want to build? (choose with the respective number):\n1 A house for 29 wood;\n"
 								"2.An animal pecuary house for 60 wood;\n3 A smelting forge for 40 stone;\n4 A crafting forge for 50 stone.\n>>  ")
 		if what_toBuild == '1':
 			if gameVars.misc_wood >= 29:
@@ -213,7 +296,7 @@ while gameVars.misc_people > 0:
 		os.system("cls")
 
 	if What_toDo == '7':
-		what_toCraft = input ("What do you want to craft? (choose with the respective number):\n"
+		what_toCraft = raw_input ("What do you want to craft? (choose with the respective number):\n"
 							  "1.A stone sword for 50 smelted stone;\n2.An iron sword for 125 smelted iron;\n3.A stone axe for 60 smelted stone;\n"
 							  "4.An iron axe for 80 smelted iron;\n5.A stone pickaxe for 130 smelted stone;\n6.An iron pickaxe for 180 smelted iron.\n>>  ")
 		if what_toCraft == '1' and gameVars.constructions_smeltingForge >= 1:
@@ -300,10 +383,10 @@ while gameVars.misc_people > 0:
 			os.system("cls")
 	
 	if What_toDo == '8':
-		what_toSmelt = input ("What do you want to smelt?:\n1.Smelt iron;\n2.Smelt stone.\n>>  ")
+		what_toSmelt = raw_input ("What do you want to smelt?:\n1.Smelt iron;\n2.Smelt stone.\n>>  ")
 		if what_toSmelt == '1':
 			if gameVars.constructions_smeltingForge >= 1 and gameVars.misc_iron >= 1:
-				howm_ironSmelt = input ("How much iron do you pretend to smelt?\n>>  ")
+				howm_ironSmelt = raw_input ("How much iron do you pretend to smelt?\n>>  ")
 				howm_ironSmelt_int = int(howm_ironSmelt)
 				if misc_iron >= howm_ironSmelt_int:
 					for number in range(howm_ironSmelt_int):
@@ -320,7 +403,7 @@ while gameVars.misc_people > 0:
 		
 		if what_toSmelt == '2':
 			if gameVars.constructions_smeltingForge >= 1 and gameVars.misc_stone >= 1:
-				howm_stoneSmelt = input ("How much stone do you intend to smelt?\n>>  ")
+				howm_stoneSmelt = raw_input ("How much stone do you intend to smelt?\n>>  ")
 				howm_stoneSmelt_int = int(howm_stoneSmelt)
 				if gameVars.misc_stone >= howm_stoneSmelt_int:
 					for number in range(howm_stoneSmelt_int):
@@ -333,6 +416,30 @@ while gameVars.misc_people > 0:
 					print("Come back when you have decided to enter less or as many stone as you have!!!")
 			elif gameVars.constructions_smeltingForge == 0 or gameVars.misc_stone == 0:
 				print("Come back when you have at least 1 stone!!! Or when you have a smelting forge...")
+	
+	if What_toDo == '9':
+		#Need to print out all of the resources to the user 
+		#Wait for the user to press a key to continue
+		print("You have:\n")
+		
+		print("\tEquipment")
+		print("\t\tStone Sword: " + str(gameVars.misc_stoneSword[0]))
+		print("\t\tIron Sword: " + str(gameVars.misc_ironSword[0]))
+		print("\t\tStone Axe: " + str(gameVars.misc_stoneAxe[0]))
+		print("\t\tIron Axe: " + str(gameVars.misc_ironAxe[0]))
+		print("\t\tStone Pickaxe: " + str(gameVars.misc_stonePickaxe[0]))
+		print("\t\tIron Pickaxe: " + str(gameVars.misc_ironPickaxe[0]))
+		
+		print("\tResources:\n")
+		print("\t\tFood: " + str(gameVars.consumables_food))
+		print("\t\tWood: " + str(gameVars.misc_wood))
+		print("\t\tStone: " + str(gameVars.misc_stone))
+		print("\t\tSmelted Stone: " + str(gameVars.misc_stoneSm))
+		print("\t\tIron: " + str(gameVars.misc_iron))
+		print("\t\tSmelted Iron: " + str(gameVars.misc_ironSm))
+		print("Press any key to continue")
+		raw_input()
+		os.system("cls")
 	print("\n")
 		
                 
--- durability.py
+++ durability.py
@@ -1,84 +1,124 @@
 from Vars import gameVars
 
-def decreaseDurability(stoneSword, ironSword, stoneAxe, ironAxe, stonePickaxe, ironPickaxe):
+def decreaseDurability(durabilityLoss, stoneSword, ironSword, stoneAxe, ironAxe, stonePickaxe, ironPickaxe):
 	if stoneSword == 1:
-		print("Stone sword Durability Check!");
-		print(gameVars.misc_stoneSword[0]);
+		#print("Stone sword Durability Check!");
+		#print(gameVars.misc_stoneSword[0]);
+		#print(gameVars.misc_stoneSword[1]);
 		#Do I have a stoneSword?
 		if(gameVars.misc_stoneSword[0] >0):
 			#reduce durability by 1
-			gameVars.misc_stoneSword[1] -= 1
+			print("Old stone sword durability: " + str(gameVars.misc_stoneSword[1]))
+			gameVars.misc_stoneSword[1] -= durabilityLoss
+			print("new stone sword durability: " + str(gameVars.misc_stoneSword[1]))
 			#if durability is <= 0...need to break the weapon...
 			if(gameVars.misc_stoneSword[1] <= 0):
+				print("Stone sword has broke!")
 				gameVars.misc_stoneSword[1] = gameVars.misc_stoneSwordDurSet
-				gamevars.misc_stoneSword[0] -= 1;
-				return True;
+				gameVars.misc_stoneSword[0] -= 1
+				print("Stone swords remaining: " + str(gameVars.misc_stoneSword[0]))
+			return True;
 		else:
 			print("False!")
 			return False
 		
 	elif ironSword == 1:
-		#Do I have a stoneSword?
+		#print("iron sword Durability Check!");
+		#print(gameVars.misc_ironSword[0]);
+		#print(gameVars.misc_ironSword[1]);
+		#Do I have a ironSword?
 		if(gameVars.misc_ironSword[0] >0):
 			#reduce durability by 1
+			print("Old iron sword durability: " + str(gameVars.misc_ironSword[1]))
 			gameVars.misc_ironSword[1] -= 1
+			print("New iron sword durability: " + str(gameVars.misc_ironSword[1]))
 			#if durability is <= 0...need to break the weapon...
 			if(gameVars.misc_ironSword[1] <= 0):
-				gameVars.misc_stoneSword[1] = gameVars.misc_ironSwordDurSet
-				gamevars.misc_ironSword[0] -= 1;
-				return True;
+				print("Iron sword has broke!")
+				gameVars.misc_ironSword[1] = gameVars.misc_ironSwordDurSet
+				gameVars.misc_ironSword[0] -= 1;
+				print("Iron swords remaining: " + str(gameVars.misc_ironSword[0]))
+			return True;
 		else:
 			return False
 		
 	elif stoneAxe == 1:
-		#Do I have a stoneSword?
+		#print("Stone axe Durability Check!");
+		#print(gameVars.misc_stoneAxe[0]);
+		#print(gameVars.misc_stoneAxe[1]);
+		#Do I have a stoneAxe?
 		if(gameVars.misc_stoneAxe[0] >0):
 			#reduce durability by 1
-			gameVars.misc_stoneAxe[1] -= 1
+			print("Old stone axe durability: " + str(gameVars.misc_stoneAxe[1]))
+			gameVars.misc_stoneAxe[1] -= durabilityLoss
+			print("New stone axe durability: " + str(gameVars.misc_stoneAxe[1]))
 			#if durability is <= 0...need to break the weapon...
 			if(gameVars.misc_stoneAxe[1] <= 0):
-				gameVars.misc_stoneSword[1] = gameVars.misc_stoneAxeDurSet
-				gamevars.misc_stoneAxe[0] -= 1;
-				return True;
+				print("Stone axe has broken!")
+				gameVars.misc_stoneAxe[1] = gameVars.misc_stoneAxeDurSet
+				gameVars.misc_stoneAxe[0] -= 1;
+				print("Stone axes remaining: " + str(gameVars.misc_stoneAxe[0]))
+			return True;
 		else:
 			return False
 	
 	elif ironAxe == 1:
-		#Do I have a stoneSword?
+		#print("Iron axe Durability Check!");
+		#print(gameVars.misc_ironAxe[0]);
+		#print(gameVars.misc_ironAxe[1]);
+		#Do I have a ironAxe?
 		if(gameVars.misc_ironAxe[0] >0):
 			#reduce durability by 1
-			gameVars.misc_ironAxe[1] -= 1
+			print("Old iron axe durability: " + str(gameVars.misc_ironAxe[1]))
+			gameVars.misc_ironAxe[1] -= durabilityLoss
+			print("Old iron axe durability: " + str(gameVars.misc_ironAxe[1]))
 			#if durability is <= 0...need to break the weapon...
 			if(gameVars.misc_ironAxe[1] <= 0):
-				gameVars.misc_stoneSword[1] = gameVars.misc_ironAxeDurSet
-				gamevars.misc_ironAxe[0] -= 1;
-				return True;
+				print("Iron pickaxe has broken!")
+				gameVars.misc_ironAxe[1] = gameVars.misc_ironAxeDurSet
+				gameVars.misc_ironAxe[0] -= 1;
+				print("Iron pickaxes remaining: " + str(gameVars.misc_ironAxe[0]))
+			return True;
 		else:
 			return False
 	
 	elif stonePickaxe == 1:
-		#Do I have a stoneSword?
+		#print("Stone Pickaxe Durability Check!");
+		#print(gameVars.misc_stonePickaxe[0]);
+		#print(gameVars.misc_stonePickaxe[1]);
+		#Do I have a stonePickaxe?
 		if(gameVars.misc_stonePickaxe[0] >0):
 			#reduce durability by 1
-			gameVars.misc_stonePickaxe[1] -= 1
+			print("Old stone pickaxe durability: " + str(gameVars.misc_stonePickaxe[1]))
+			gameVars.misc_stonePickaxe[1] -= durabilityLoss
+			print("Old stone pickaxe durability: " + str(gameVars.misc_stonePickaxe[1]))
 			#if durability is <= 0...need to break the weapon...
 			if(gameVars.misc_stonePickaxe[1] <= 0):
-				gameVars.misc_stoneSword[1] = gameVars.misc_stonePickaxeDurSet
-				gamevars.misc_stonePickaxe[0] -= 1;
-				return True;
+				print("Stone pickaxe has broken!")
+				gameVars.misc_stonePickaxe[1] = gameVars.misc_stonePickaxeDurSet
+				gameVars.misc_stonePickaxe[0] -= 1;
+				print("Stone pickaxes remaining: " + str(gameVars.misc_stonePickaxe[0]))
+			return True;
 		else:
 			return False
 	
 	elif ironPickaxe == 1:
-		#Do I have a stoneSword?
+		#print("Iron Pickaxe Durability Check!");
+		#print(gameVars.misc_ironPickaxe[0]);
+		#print(gameVars.misc_ironPickaxe[1]);
+		#Do I have a ironPickaxe?
 		if(gameVars.misc_ironPickaxe[0] >0):
 			#reduce durability by 1
-			gameVars.misc_ironPickaxe[1] -= 1
+			print("Old iron pickaxe durability: " + str(gameVars.misc_ironPickaxe[1]))
+			gameVars.misc_ironPickaxe[1] -= durabilityLoss
+			print("New iron pickaxe durability: " + str(gameVars.misc_ironPickaxe[1]))
 			#if durability is <= 0...need to break the weapon...
 			if(gameVars.misc_ironPickaxe[1] <= 0):
-				gameVars.misc_stoneSword[1] = gameVars.misc_ironPickaxeDurSet
-				gamevars.misc_ironPickaxe[0] -= 1;
-				return True;
+				print("Iron pickaxe has broken!")
+				gameVars.misc_ironPickaxe[1] = gameVars.misc_ironPickaxeDurSet
+				gameVars.misc_ironPickaxe[0] -= 1;
+				print("Iron pickaxes remaining: " + str(gameVars.misc_ironPickaxe[0]))
+			return True;
 		else:
 			return False
 	
